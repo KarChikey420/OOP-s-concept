00933862edcdd9bc2b62e913a1abc23f5ac3c309
KarChikey <kartikeynegi2002@gmail.com>
2025-09-08 19:32:02 +0530

    better way

--- Abstraction.py
+++ Abstraction.py
@@ -1,43 +1,46 @@
-from abc import ABC,abstractmethod
+from abc import ABC , abstractmethod
+
+pi=3.14
 
 class Shape(ABC):
     
     @abstractmethod
-    
     def area(self):
         pass
     
     @abstractmethod
-    
     def perimeter(self):
         pass
-    
-class Circle(Shape):
-    def __init__(self,r):
-        self.r=r   
-    
-    def area(self):
-        return 3.14 * self.r * self.r
-    
-    def perimeter(self):
-        return 2 * 3.14 * self.r
-        
+
 class ractangle(Shape):
-    def __init__(self,lenght,width):
-        self.lenght=lenght
-        self.width=width
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    
+    def area(self):
+        return f"The area of a ractangle:{self.a * self.b}"
+    
+    def perimeter(self):
+        return f"The perimeter of a ractangle:{2*(self.a+self.b)}"
+        
+class circle(Shape):
+    def __init__(self,r):
+        self.r=r
         
     def area(self):
-        return self.lenght * self.width
+        return f"The area of a circle:{pi*self.r*self.r}"
         
     def perimeter(self):
-        return 2 * ( self.lenght + self.width)
-        
-obj1=Circle(4)
-obj2=ractangle(2,3)
+        return f"The perimeter of a circle:{2*pi*self.r}"
 
-print(f"The area of Circle:{obj1.area()}")
-print(f"The perimeter of Circle:{obj1.perimeter()}")
+obj=ractangle(2,3)
+print(obj.area())
+print(obj.perimeter())
 
-print(f"The area of ractangle:{obj2.area()}")
-print(f"The peramiter of ractangle:{obj2.perimeter()}")
+obj1=circle(3)
+print(obj1.area())
+print(obj1.perimeter())
+    
+    
+    
+    
