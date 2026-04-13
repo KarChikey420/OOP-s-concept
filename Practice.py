4e4ab415fb227f8c47dbd0b04e60274fc36a68a0
KarChikey <kartikeynegi2002@gmail.com>
2026-04-13 22:37:38 +0530

    add

--- Practice.py
+++ Practice.py
@@ -1,20 +1,20 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 
-class Payment:
-    def __init__(self,method_name):
-        self.method_name=method_name
-        
+class Payment(ABC):
+    def __init__(self, method_name):
+        self.method_name = method_name
+    
     @abstractmethod
-    def pay(self,amount):
+    def process_payment(slef, amount):
         pass
-
+    
 class CreditCardPayment(Payment):
     def __init__(self, method_name):
         super().__init__(method_name)
     
-    def pay(self,amount):
-        print(f"Using {self.method_name} to pay amount {amount}")
-    
-if __name__ == "__main__":
-    cc=CreditCardPayment("credit_card")
-    cc.pay(1000)
+    def process_payment(self, amount):
+        print(f"Using {self.method_name} to process payment of amount {amount}")
+
+if __name__ =="__main__":
+    cc = CreditCardPayment("Credit Card")
+    cc.process_payment(1000)
