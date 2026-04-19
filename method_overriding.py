79a8a1346f3fe0b3602274fa368038f15703e082
KarChikey <kartikeynegi2002@gmail.com>
2026-04-19 23:38:35 +0530

    method overriding2

--- method_overriding.py
+++ method_overriding.py
@@ -1,22 +1,10 @@
-class addition:
-    def __init__(self, a, b):
-        self.a=a
-        self.b=b
-        
-    def add(self):
-        return self.a + self.b
+class Animal:
+    def dog(self):
+        print("bark slowly")
 
-class submision(addition):
-    def __init__(self,a,b,c):
-        super().__init__(a,b)
-        self.c=c
-        
-    def add(self):
-        return self.a + self.b + self.c
+class Animal2(Animal):
+    def dog(self):
+        print("bark loudly")
+
+
     
-if __name__ == "__main__":
-    myobj=addition(1,2)
-    print(myobj.add())
-    
-    myobj1=submision(1,2,3)
-    print(myobj1.add())
